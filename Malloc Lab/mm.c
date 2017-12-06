4f9a6df6f48752cf4446b53c5aa56deee47806b5
zyf0726 <940104357@qq.com>
2017-12-06 11:51:12 +0800

    Malloc Lab v1.3

--- Malloc Lab/mm.c
+++ Malloc Lab/mm.c
@@ -29,7 +29,7 @@
 
 /* If you want debugging output, use the following macro.  When you hand
  * in, remove the #define DEBUG line. */
-//#define DEBUG
+#define NDEBUG
 #ifdef DEBUG
 # define dbg_printf(...) printf(__VA_ARGS__)
 #else
@@ -55,8 +55,6 @@
 #define WSIZE       	4       	/* Word and header/footer size (bytes) */ 
 #define DSIZE       	8       	/* Double word size (bytes) */
 #define MIN_BLKSIZE		((DSIZE)*2)	/* Minimal block size in heap */
-
-#define MAX(x, y) ((x) > (y)? (x) : (y))  
 
 /* Pack a size and allocated bits into a word */
 #define PACK(size, prev_alloc, alloc)  ((size) | ((prev_alloc) << 1) | (alloc)) 
@@ -85,38 +83,59 @@
 #define NEXT_BLKP(bp)  ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) 
 #define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE))) 
 
-/* head/cur node of each list */	/*   words				policy 			*/
-static void *head_1, *cur_1,		/*   1 ~  4			LIFO + next_fit		*/
-		    *head_2, *cur_2,		/*   5 ~  6				....			*/
-			*head_3, *cur_3,		/*   7 ~  8				....			*/
-			*head_4, *cur_4,		/*   9 ~ 10				....			*/
-			*head_5, *cur_5,		/*   11 ~ 12			....			*/
-			*head_6, *cur_6,		/*   13 ~ 14			....			*/
-			*head_7, *cur_7,		/*   15 ~ 16			....			*/
-			*head_8, *cur_8,		/*   17 ~ 18			....			*/
-			*head_9, *cur_9,		/*   19 ~ 20			....			*/		
-			*head_10, *cur_10,		/*	 21 ~ 32			....			*/
-			*head_11, *cur_11,		/*   33 ~ 40			....			*/
-			*head_12, *cur_12,		/*   41 ~ 48			....			*/
-			*head_13, 				/*   49 ~ 64		LIFO + first_fit	*/
-			*head_14,				/*   65 ~ 128			....			*/
-			*head_15,				/*	129 ~ 216			....			*/
-			*head_16,				/*	217 ~ 448			....			*/
-			*head_17,				/*	449 ~ 768			....			*/
-			*head_18,				/*  769 ~ 1536		ADDR + first_fit	*/
-			*head_19,				/*  1537 ~ 4096			....			*/
-			*head_20;				/*  4097 ~ inf			....			*/
-					
-#define LIFO_NXTFIT			0x2
-#define LIFO_FSTFIT			0x3
-#define ADDR_FSTFIT			0x1
-#define GET_POLICY(bid)		(((bid) <= 12) ? (LIFO_NXTFIT) :\
-							 ((bid) <= 17) ? (LIFO_FSTFIT) :\
-							 (ADDR_FSTFIT))
-#define IS_NXTFIT(code)		(!((code) & 1))
-#define IS_FSTFIT(code)		((code) & 1)
-#define IS_LIFO(code)		((code) >> 1)
-#define IS_ADDR(code)		(!((code) >> 1))
+struct head_table {
+	void *h0, *h1, *h2, *h3, *h4, *h5;
+	void *h6, *h7, *h8, *h9, *h10, *h11;
+	void *h12, *h13, *h14, *h15, *h16;
+	void *h17, *h18, *h19, *h20;
+};
+static struct head_table head_table;
+static void **head_t = &head_table.h0;
+
+struct cur_table {
+	void *c0;
+};
+static struct cur_table cur_table;
+static void **cur_t = &cur_table.c0;
+
+/************************************  
+	 words				policy 			
+	1 ~  4			LIFO + first_fit	
+	5 ~  6				....			
+	7 ~  8				....			
+	9 ~ 10				....			
+	11 ~ 12				....			
+	13 ~ 14				....			
+	15 ~ 16				....			
+	17 ~ 18				....			
+	19 ~ 20				....					
+	21 ~ 32				....			
+	33 ~ 40				....			
+	41 ~ 48				....			
+	49 ~ 64				....			
+	65 ~ 128			....			
+	129 ~ 216			....			
+	217 ~ 448			....			
+	449 ~ 768			....			
+	769 ~ 1536		LIFO + best_fit		
+	1537 ~ 4096			....			
+	4097 ~ inf			....
+*************************************/
+#define NXTFIT			0x1
+#define FSTFIT			0x2
+#define BSTFIT			0x3
+#define LIFO			0x4
+#define ADDR			0x5
+#define TREE			0x6
+#define IS_NXTFIT(c)		(((c) & 3) == NXTFIT)
+#define IS_FSTFIT(c)		(((c) & 3) == FSTFIT)
+#define IS_BSTFIT(c)		(((c) & 3) == BSTFIT)
+#define IS_LIFO(c)			(((c) & 12) == LIFO)
+#define IS_ADDR(c)			(((c) & 12) == ADDR)
+#define IS_TREE(c)			(((c) & 12) == TREE)
+#define GET_POLICY(bid)		(((bid) <= 13) ? (LIFO | FSTFIT) :\
+							 ((bid) <= 17) ? (LIFO | FSTFIT) :\
+							 (LIFO | BSTFIT))
 #define BUCKET_CNT			20
 
 static void *EPLG_BP = NULL;				/* epilogue block pointer */
@@ -124,140 +143,31 @@ static void *PRLG_BP = NULL;				/* prologue block pointer */
 					
 /* Given size, find which bucket it fits */
 static inline int GET_BUCKETID(int size) {
-	if (size >= 1 && size <= 4) return 1;
-	if (size >= 5 && size <= 6) return 2;
-	if (size >= 7 && size <= 8) return 3;
-	if (size >= 9 && size <= 10) return 4;
-	if (size >= 11 && size <= 12) return 5;
-	if (size >= 13 && size <= 14) return 6;
-	if (size >= 15 && size <= 16) return 7;
-	if (size >= 17 && size <= 18) return 8;
-	if (size >= 19 && size <= 20) return 9;
-	if (size >= 21 && size <= 32) return 10;
-	if (size >= 33 && size <= 40) return 11;
-	if (size >= 41 && size <= 48) return 12;
-	if (size >= 49 && size <= 64) return 13;
-	if (size >= 65 && size <= 128) return 14;
-	if (size >= 129 && size <= 216) return 15;
-	if (size >= 217 && size <= 448) return 16;
-	if (size >= 449 && size <= 768) return 17;
-	if (size >= 769 && size <= 1536) return 18;
-	if (size >= 1537 && size <= 4096) return 19;
-	if (size >= 4097) return 20;
+	if (size >= 1 && size <= 4) return 0;
+	if (size >= 5 && size <= 6) return 1;
+	if (size >= 7 && size <= 8) return 2;
+	if (size >= 9 && size <= 10) return 3;
+	if (size >= 11 && size <= 12) return 4;
+	if (size >= 13 && size <= 14) return 5;
+	if (size >= 15 && size <= 16) return 6;
+	if (size >= 17 && size <= 18) return 7;
+	if (size >= 19 && size <= 20) return 8;
+	if (size >= 21 && size <= 32) return 9;
+	if (size >= 33 && size <= 40) return 10;
+	if (size >= 41 && size <= 48) return 11;
+	if (size >= 49 && size <= 64) return 12;
+	if (size >= 65 && size <= 128) return 13;
+	if (size >= 129 && size <= 216) return 14;
+	if (size >= 217 && size <= 448) return 15;
+	if (size >= 449 && size <= 768) return 16;
+	if (size >= 769 && size <= 1536) return 17;
+	if (size >= 1537 && size <= 4096) return 18;
+	if (size >= 4097) return 19;
 	dbg_printf("GET_BUCKETID error: size(%d) doesn't fit any bucket.\n", size);
 	return 0;
 }
 static inline int FIT_BUCKET(int bid, int size) {
-	switch (bid) {
-		case 1: return size >= 1 && size <= 4;
-		case 2: return size >= 5 && size <= 6;
-		case 3: return size >= 7 && size <= 8;
-		case 4: return size >= 9 && size <= 10;
-		case 5: return size >= 11 && size <= 12;
-		case 6:	return size >= 13 && size <= 14;
-		case 7: return size >= 15 && size <= 16;
-		case 8: return size >= 17 && size <= 18;
-		case 9: return size >= 19 && size <= 20;
-		case 10: return size >= 21 && size <= 32;
-		case 11: return size >= 33 && size <= 40;
-		case 12: return size >= 41 && size <= 48;
-		case 13: return size >= 49 && size <= 64;
-		case 14: return size >= 65 && size <= 128;
-		case 15: return size >= 129 && size <= 216;
-		case 16: return size >= 217 && size <= 448;
-		case 17: return size >= 449 && size <= 768;
-		case 18: return size >= 769 && size <= 1536;
-		case 19: return size >= 1537 && size <= 4096;
-		case 20: return size >= 4097;
-	}
-	dbg_printf("FIT_BUCKET error: bid(%d) illegal.\n", bid);
-	return 0;
-}
-static inline void *LIST_HEAD(int bid) {
-	switch (bid) {
-		case 1:	return head_1;
-		case 2:	return head_2;
-		case 3:	return head_3;
-		case 4:	return head_4;
-		case 5:	return head_5;
-		case 6:	return head_6;
-		case 7: return head_7;
-		case 8: return head_8;
-		case 9: return head_9;
-		case 10: return head_10;
-		case 11: return head_11;
-		case 12: return head_12;
-		case 13: return head_13;
-		case 14: return head_14;
-		case 15: return head_15;
-		case 16: return head_16;
-		case 17: return head_17;	
-		case 18: return head_18;	
-		case 19: return head_19;	
-		case 20: return head_20;	
-	}
-	dbg_printf("LIST_HEAD error: bid(%d) illegal.\n", bid);
-	return 0;
-}
-static inline void *LIST_CUR(int bid) {
-	switch (bid) {
-		case 1:	return cur_1;
-		case 2: return cur_2;
-		case 3: return cur_3;
-		case 4: return cur_4;
-		case 5: return cur_5;
-		case 6:	return cur_6;
-		case 7: return cur_7;
-		case 8: return cur_8;
-		case 9: return cur_9;
-		case 10: return cur_10;
-		case 11: return cur_11;
-		case 12: return cur_12;
-	}
-	dbg_printf("LIST_HEAD error: bid(%d) illegal.\n", bid);
-	return 0;
-}
-static inline void SET_HEAD(int bid, void *head) {
-	switch (bid) {
-		case 1:	head_1 = head; return;
-		case 2:	head_2 = head; return;
-		case 3:	head_3 = head; return;
-		case 4:	head_4 = head; return;
-		case 5:	head_5 = head; return;
-		case 6:	head_6 = head; return;
-		case 7: head_7 = head; return;
-		case 8: head_8 = head; return;
-		case 9: head_9 = head; return;
-		case 10: head_10 = head; return;
-		case 11: head_11 = head; return;
-		case 12: head_12 = head; return;
-		case 13: head_13 = head; return;
-		case 14: head_14 = head; return;
-		case 15: head_15 = head; return;
-		case 16: head_16 = head; return;
-		case 17: head_17 = head; return;
-		case 18: head_18 = head; return;
-		case 19: head_19 = head; return;
-		case 20: head_20 = head; return;
-	}
-	dbg_printf("SET_HEAD error: bid(%d) illegal.\n", bid);
-}
-static inline void SET_CUR(int bid, void *cur) {
-	switch (bid) {
-		case 1:	cur_1 = cur; return;
-		case 2: cur_2 = cur; return;
-		case 3: cur_3 = cur; return;
-		case 4: cur_4 = cur; return;
-		case 5: cur_5 = cur; return;
-		case 6:	cur_6 = cur; return;
-		case 7: cur_7 = cur; return;
-		case 8: cur_8 = cur; return;
-		case 9: cur_9 = cur; return;
-		case 10: cur_10 = cur; return;
-		case 11: cur_11 = cur; return;
-		case 12: cur_12 = cur; return;
-	}
-	dbg_printf("SET_CUR error: bid(%d) illegal.\n", bid);
+	return bid == GET_BUCKETID(size);
 }
 
 /* Function prototypes for internal helper routines */
@@ -286,10 +196,10 @@ int mm_init(void) {
 	PRLG_BP = PRLG_BP + (2*WSIZE);		/* Prologue block pointer */
 	dbg_printf("\nPRLG_BP = %p\tEPLG_BP = %p\n", PRLG_BP, EPLG_BP);
 	
-	for (int i = 1; i <= BUCKET_CNT; ++i) { /* initialize list */
-		SET_HEAD(i, PRLG_BP);
+	for (int i = 0; i < BUCKET_CNT; ++i) { /* initialize list */
+		head_t[i] = PRLG_BP;
 		if (IS_NXTFIT(GET_POLICY(i)))
-			SET_CUR(i, PRLG_BP);
+			cur_t[i] = PRLG_BP;
 	}
 	
     return 0;
@@ -317,7 +227,7 @@ void *malloc (size_t size) {
 	
 	/* Search the free list for a fit */
 	bid = GET_BUCKETID(asize);
-	for (i = bid; i <= BUCKET_CNT; ++i) {
+	for (i = bid; i < BUCKET_CNT; ++i) {
 		bp = find_fit(i, asize);
 		if (bp != NULL) {
 			place(bp, asize);
@@ -468,8 +378,8 @@ void mm_checkheap(int lineno) {
 		if (!GET_ALLOC(HDRP(bp))) ++free_cnt;
 	}
 	
-	for (int i = 1; i <= BUCKET_CNT; ++i) {
-		for (bp = LIST_HEAD(i); bp != PRLG_BP; bp = GET_ADDR(LIST_NEXT(bp))) {
+	for (int i = 0; i < BUCKET_CNT; ++i) {
+		for (bp = head_t[i]; bp != PRLG_BP; bp = GET_ADDR(LIST_NEXT(bp))) {
 			/* Check all free list pointers */
 			if (!in_heap(bp)) {
 				dbg_printf("Heap Checker Error(line %d): "
@@ -514,7 +424,7 @@ void mm_checkheap(int lineno) {
  */
 static void *extend_heap(size_t words) 
 {
-	char *bp, pa; size_t size;
+	char *bp, prev_alloc; size_t size;
 	
 	/* Allocate an even number of words to maintain alignment */
 	size = (words + (words & 1)) * WSIZE;
@@ -523,14 +433,11 @@ static void *extend_heap(size_t words)
 		return NULL;
 	
 	/* Initialize free block header/footer and the epilogue header */
-	bp = EPLG_BP, pa = GET_PREVALLOC(HDRP(bp));
-	PUT(HDRP(bp), PACK(size, pa, 0));		/* Free block header */   
-	PUT(FTRP(bp), PACK(size, pa, 0));		/* Free block footer */
+	bp = EPLG_BP, prev_alloc = GET_PREVALLOC(HDRP(bp));
+	PUT(HDRP(bp), PACK(size, prev_alloc, 0));		/* Free block header */   
+	PUT(FTRP(bp), PACK(size, prev_alloc, 0));		/* Free block footer */
 	EPLG_BP = NEXT_BLKP(bp);
 	PUT(HDRP(EPLG_BP), PACK(0, 0, 1));		/* Update epilogue block*/
-//	dbg_printf("PRLG_BP = %p  EPLG_BP=%p\n", PRLG_BP, EPLG_BP);		
-//	for (void *bp = PRLG_BP - WSIZE; bp != EPLG_BP; bp += WSIZE)
-//		dbg_printf("*%p = %u | %u | %u\n", bp, GET_SIZE(bp), GET_PREVALLOC(bp), GET_ALLOC(bp));
 	
     /* Coalesce if the previous block was free */
     bp = coalesce(bp);
@@ -549,8 +456,6 @@ static void *coalesce(void *bp) {
 	size_t next_size = GET_SIZE(HDRP(NEXT_BLKP(bp)));
 	
 	int bid = GET_BUCKETID(size), prev_bid, next_bid;
-	
-	dbg_printf("coalesce(%p) size=%ld bid=%d\n", bp, size, bid);
 	
 	if (prev_alloc && next_alloc) {			/* Case 1 */
 		list_insert(bid, bp);
@@ -616,9 +521,9 @@ static void place(void *bp, size_t asize) {
 		list_erase(bid, bp);
 		bp = NEXT_BLKP(bp);
 		size -= asize;
-		bid = GET_BUCKETID(size);
 		PUT(HDRP(bp), PACK(size, 1, 0));
 		PUT(FTRP(bp), PACK(size, 1, 0));
+		bid = GET_BUCKETID(size);
 		list_insert(bid, bp);
 	}
 	else {
@@ -641,34 +546,47 @@ static void place(void *bp, size_t asize) {
  *			  using next-fit or first-fit policy
  */
 static void *find_fit(int bid, size_t asize) {
-	int policy = GET_POLICY(bid); void *bp, *oldbp;
+	int policy = GET_POLICY(bid);
+	void *bp, *oldbp, *bestbp;
 	if (IS_NXTFIT(policy)) {
 		/* Next fit search */
-		oldbp = LIST_CUR(bid);
+		oldbp = cur_t[bid];
 		
 		/* search from the current-pointer to the end of list */
 		for (bp = oldbp; bp != PRLG_BP; bp = GET_ADDR(LIST_NEXT(bp)))
 			if (!GET_ALLOC(bp) && (asize <= GET_SIZE(HDRP(bp)))) {
-				SET_CUR(bid, bp);
+				cur_t[bid] = bp;
 				return bp;
 			}
 				
 		/* search from start of list to old current-pointer */
-		for (bp = LIST_HEAD(bid); bp != oldbp; bp = GET_ADDR(LIST_NEXT(bp)))
+		for (bp = head_t[bid]; bp != oldbp; bp = GET_ADDR(LIST_NEXT(bp)))
 			if (!GET_ALLOC(bp) && (asize <= GET_SIZE(HDRP(bp)))) {
-				SET_CUR(bid, bp);
+				cur_t[bid] = bp;
 				return bp;
 			}		
 		
 		return NULL;	/* no fit found */
 	}
-	else {
+	else if (IS_FSTFIT(policy)) {
 		/* First-fit search */
-		for (bp = LIST_HEAD(bid); bp != PRLG_BP; bp = GET_ADDR(LIST_NEXT(bp)))
+		for (bp = head_t[bid]; bp != PRLG_BP; bp = GET_ADDR(LIST_NEXT(bp)))
 			if (!GET_ALLOC(bp) && (asize <= GET_SIZE(HDRP(bp))))
 				return bp;
 		return NULL;	/* no fit found */
 	}
+	else if (IS_BSTFIT(policy)) {
+		/* Best-fit search */
+		bestbp = NULL;
+		for (bp = head_t[bid]; bp != PRLG_BP; bp = GET_ADDR(LIST_NEXT(bp)))
+			if (!GET_ALLOC(bp) && asize <= GET_SIZE(HDRP(bp)) &&
+				(!bestbp || GET_SIZE(HDRP(bp)) < GET_SIZE(HDRP(bestbp)))) {
+				bestbp = bp;	
+			}
+		return bestbp;
+	}
+	dbg_printf("find_fit error: unknown bid or policy code.\n");
+	return NULL;
 }
 
 /*
@@ -679,32 +597,33 @@ static void list_insert(int bid, void *bp) {
 	int policy = GET_POLICY(bid);
 	void *head, *cur_bp, *next_bp;
 	if (IS_LIFO(policy)) {	/* last-in-first-out */
-		head = LIST_HEAD(bid);
+		head = head_t[bid];
 		if (head == PRLG_BP) {
 			PUT(LIST_PREV(bp), 0);
 			PUT(LIST_NEXT(bp), 0);
-			SET_HEAD(bid, bp);
-			SET_CUR(bid, bp);
+			head_t[bid] = bp;
+			if (IS_NXTFIT(policy))
+				cur_t[bid] = bp;
 			return;			
 		}
 		PUT(LIST_PREV(head), bp - PRLG_BP);
 		PUT(LIST_NEXT(bp), head - PRLG_BP);
 		PUT(LIST_PREV(bp), 0);
-		SET_HEAD(bid, bp);
+		head_t[bid] = bp;
 	}
 	else {	/* address-order */
-		head = LIST_HEAD(bid);
-		if (head == PRLG_BP) {
+		head = head_t[bid];
+		if (head == PRLG_BP) { /* empty list */
 			PUT(LIST_PREV(bp), 0);
 			PUT(LIST_NEXT(bp), 0);
-			SET_HEAD(bid, bp);
+			head_t[bid] = bp;
 			return;
 		}
 		if (bp < head) {	/* insert at list_head */
 			PUT(LIST_PREV(head), bp - PRLG_BP);
 			PUT(LIST_NEXT(bp), head - PRLG_BP);
 			PUT(LIST_PREV(bp), 0);
-			SET_HEAD(bid, bp);
+			head_t[bid] = bp;
 			return;
 		}
 		/* insert inside the list */
@@ -736,8 +655,8 @@ static void list_erase(int bid, void *bp) {
 	PUT(LIST_NEXT(prev_bp), next_bp - PRLG_BP);
 	PUT(LIST_PREV(next_bp), prev_bp - PRLG_BP);
 	if (prev_bp == PRLG_BP)
-		SET_HEAD(bid, next_bp);
+		head_t[bid] = next_bp;
 	/*Make sure the current-pointer isn't pointing into a erased block */
-	if (IS_NXTFIT(policy) && bp == LIST_CUR(bid))
-		SET_CUR(bid, next_bp);
+	if (IS_NXTFIT(policy) && bp == cur_t[bid])
+		cur_t[bid] = next_bp;
 }
